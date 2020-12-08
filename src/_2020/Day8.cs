dd2a041b2b72fad6f9d61a53566dadc9b4a4c23d
Sam Welek <s.welek@outlook.com>
2020-12-08 20:53:15 +0000

    The big comment cleanup

---
+++ src/_2020/Day8.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace AdventOfCode._2020
+{
+    class Day8 : DayBase
+    {
+        private readonly string _input = Program.GetInput(2020, 8);
+
+        /// <summary>
+        /// --- Day 8: Handheld Halting ---
+        /// </summary>
+        public Day8()
+        {
+
+        }
+
+        /// <summary>
+        /// --- Day 8: Handheld Halting; Part A ---
+        /// </summary>
+        private protected override string PartA()
+        {
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// --- Day 8: Handheld Halting; Part B ---
+        /// </summary>
+        private protected override string PartB()
+        {
+            return string.Empty;
+        }
+    }
+}
